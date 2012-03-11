ec97b37d971ce1d390bb0ea177a55c4f5383510a
kamal.coder <vishnu.iitm@gmail.com>
2012-03-11 18:44:20 -0400

    adding ccup

--- CPPUtil/src/driver.cpp
+++ CPPUtil/src/driver.cpp
@@ -6,16 +6,19 @@
 #include "ccup.h"
 
 using namespace std;
-int main() {
-	cout << "hello" << endl;
+int main(){
+	cout<<"hello"<<endl;
 	//listAlgosDriver();
-	unsigned int input[] = { 1, 5, 2, 4 }; //provide the inputlist here
+	/*unsigned int input[] = {5,4,2,1}; //provide the inputlist here
 
-	cout << maxWindowBruteForce(input, sizeof(input) / sizeof(input[0]))
-			<< endl;
-	cout << maxWindowOrderN(input, sizeof(input) / sizeof(input[0])) << endl;
-	cout << maxWindowOrderNWithLists(input, sizeof(input) / sizeof(input[0]))
-			<< endl;
+	    cout<<maxWindowBruteForce(input, sizeof(input)/sizeof(input[0]))<<endl;
+	    cout<<maxWindowOrderN(input, sizeof(input)/sizeof(input[0]))<<endl;
+	    cout<<maxWindowOrderNWithLists(input, sizeof(input)/sizeof(input[0]))<<endl;
+*/
+	//int array[5][5] = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 },
+		//	{ 16, 17, 18, 19, 20 }, { 21, 22, 23, 24, 25 } };
 
+//	spiralTraverse((int**)array,5,5,0);
+	cout<<"world";
 	return 0;
 }
