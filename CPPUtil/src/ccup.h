cdda224848d27341e470cb21e9aee0fbba7518ee
Kamal Coder <vishnu.iitm@gmail.com>
2012-03-11 22:50:30 -0400

    updated spiral traversal

--- CPPUtil/src/ccup.h
+++ CPPUtil/src/ccup.h
@@ -8,7 +8,7 @@
 #include<iostream>
 using namespace std;
 
-void spiralTraverse(int** array, int rows, int cols, int depth){
+void spiralTraverse(int array[5][5], int rows, int cols, int depth){
 
 	if (rows <= 0 || cols <=0){
 		return;
@@ -16,39 +16,40 @@ void spiralTraverse(int** array, int rows, int cols, int depth){
 
 	if(rows == 1)
 	{
-		for(int i=depth; i<cols; i++)
-			cout<<array[0][i];
+		for(int i=0; i<cols; i++)
+			cout<<array[depth][depth+i];
 		return;
 	}
 
 	if (cols == 1){
 		//no cols to  print
 
-		for(int i=depth; i<rows; i++)
-			cout<<array[i][0];
+		for(int i=0; i<rows; i++)
+			cout<<array[depth+i][depth];
 		return;
 	}
 
-	for(int i=depth; i<cols; i++)
+	for(int i=0; i<cols-1; i++)
 	{
-		cout<<array[rows-1][i];
+		cout<<" "<<array[depth][depth+i];
 	}
 
-	for(int i=depth+1; i<rows; i++)
+	for(int i=0; i<rows-1; i++)
 	{
-		cout<<array[i][cols-1];
+		cout<<" "<<array[depth+i][depth+cols-1];
 	}
 
-	for(int i=depth+1; i<cols; i++)
+	for(int i=0; i<cols-1; i++)
 	{
-		cout<<array[rows-1][cols-1-i];
+		cout<<" "<<array[depth+rows-1][depth+cols-1-i];
 	}
 
-	for(int i=depth+1; i<rows; i++)
+	for(int i=0; i<rows-1; i++)
 	{
-		cout<<array[rows-1-i][cols-1];
+		cout<<" "<<array[depth+rows-1-i][depth];
 	}
 
+	cout<<" \n ";
 	spiralTraverse(array, rows-2, cols-2, depth+1);
 
 	/*
--- CPPUtil/src/driver.cpp
+++ CPPUtil/src/driver.cpp
@@ -18,7 +18,7 @@ int main(){
 	int array[5][5] = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 },
 			{ 16, 17, 18, 19, 20 }, { 21, 22, 23, 24, 25 } };
 
-	spiralTraverse((int**)array,5,5,0);
+	spiralTraverse(array,5,5,0);
 	cout<<"world";
 	return 0;
 }
